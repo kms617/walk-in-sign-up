cfb4b8f0eda0f1b62a3a8b421299e97518740a49
Kristin <krysia.codes@gmail.com>
2015-08-26 02:35:15 -0400

    Allows IFrame options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  config.action_dispatch.default_headers = { 'X-Frame-Options' => 'ALLOWALL' }
 
   # config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 end
