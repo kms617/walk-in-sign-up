1cfe3e025608c53f9236b69886a9bb0c21498fa0
Kristin <krysia.codes@gmail.com>
2015-08-26 02:10:53 -0400

    WIP

--- config/application.rb
+++ config/application.rb
@@ -44,9 +44,8 @@ module InfoKiosk
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
-    config.active_job.queue_adapter = :delayed_job
     config.assets.initialize_on_precompile = false
+    config.active_record.raise_in_transactional_callbacks = true
+    config.active_job.queue_adapter = :delayed_job
   end
 end
