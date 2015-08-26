1325e264432bc3805f9cc7daf84507d177c869a6
Kristin <krysia.codes@gmail.com>
2015-08-26 00:54:28 -0400

    Dropdown modified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,4 +31,33 @@ module ApplicationHelper
   def trade_specialties
     Specialty.where(category_id: 4).map { |specialty| [specialty.name]}
   end
+
+  def specialties_for_select
+    specialties = []
+    admin = ['Administrative']
+    admin2 = make_array(1)
+    admin << admin2
+    specialties << admin
+    contacts = ['Contact Center']
+    contact2 = make_array(2)
+    contacts << contact2
+    specialties << contacts
+    industry = ["Industrial"]
+    indust2 = make_array(3)
+    industry << indust2
+    specialties << industry
+    skill = ["Skilled Trades"]
+    skill2 = make_array(4)
+    skill << skill2
+    specialties << skill
+    specialties
+  end
+
+  def make_array(i)
+    array = []
+    Specialty.where(category_id: i).each do |specialty|
+      array << specialty.name
+    end
+    array
+  end
 end
