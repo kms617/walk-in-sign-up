0da7210f4b153d06e78215a92183f87f760e6478
Kristin <krysia.codes@gmail.com>
2015-08-26 02:32:17 -0400

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   # config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 end
